fb4c8a41fd36c10a206e07e039987ffcb7219602
Alexandre Oliveira <alexandre_osvaldo@hotmail.com>
2025-03-29 15:17:10 -0300

    build fixes

--- src/components/Forms/User/components/dataRegister/index.tsx
+++ src/components/Forms/User/components/dataRegister/index.tsx
@@ -78,7 +78,7 @@ export default function DataRegister({
 		}
 	};
 
-	const validateEmail = (email) => {
+	const validateEmail = (email: any) => {
 		const emailRegex =
 			/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 		return String(email).toLowerCase().match(emailRegex);
