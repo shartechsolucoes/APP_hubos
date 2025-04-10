85983764975f872ef6d090b9e5019cd97e6c1d03
Alexandre Oliveira <alexandre_osvaldo@hotmail.com>
2025-04-10 20:54:58 -0300

    remove unused icons

--- src/components/ListItem/Kits/index.tsx
+++ src/components/ListItem/Kits/index.tsx
@@ -1,40 +1,31 @@
-import {BsEyeFill, BsFillPencilFill, BsFillTrashFill} from 'react-icons/bs';
+import { BsFillPencilFill, BsFillTrashFill } from 'react-icons/bs';
 import { Link } from 'react-router';
 import './index.css';
 import useAccessLevelStore from '../../../stores/accessLevelStore.ts';
 
 export default function ListItemOrders({
-										   title,
-										   id,
-										   deleteListItem,
-									   }: {
+	title,
+	id,
+	deleteListItem,
+}: {
 	title?: string;
 	id?: string;
 	deleteListItem?: () => void;
 }) {
 	const { accessLevel } = useAccessLevelStore();
 	return (
-		<div className="" style={{borderBottom: "1px solid #f1efef"}}>
+		<div className="" style={{ borderBottom: '1px solid #f1efef' }}>
 			<div className="row px-4 py-3">
 				<div className="col-sm-10 col-md-10 d-flex justify-content-start align-items-center">
-					<a href="app-ecommerce-order-details.html"><span>{title}</span></a>
+					<a href="app-ecommerce-order-details.html">
+						<span>{title}</span>
+					</a>
 				</div>
 
-				<div className="col-md-2 d-flex justify-content-end align-items-center gap-3">{accessLevel === 2 ||
-					(accessLevel === 0 && (
-						<Link to={`view?id=${id}`}>
-							<BsEyeFill/>
-						</Link>
-					))}
-
+				<div className="col-md-2 d-flex justify-content-end align-items-center gap-3">
 					{accessLevel === 0 && (
 						<Link to={`form?id=${id}`}>
-							<BsFillPencilFill/>
-						</Link>
-					)}
-					{accessLevel === 2 && (
-						<Link to={`view?id=${id}`}>
-							<BsEyeFill />
+							<BsFillPencilFill />
 						</Link>
 					)}
 					{accessLevel === 0 && (
@@ -45,5 +36,5 @@ export default function ListItemOrders({
 				</div>
 			</div>
 		</div>
-		);
-		}
+	);
+}
