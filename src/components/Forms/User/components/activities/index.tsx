1b027f1557b8b9b1edaa4d0baa4df77ae38823c3
Alexandre Oliveira <alexandre_osvaldo@hotmail.com>
2025-04-19 09:36:14 -0300

    add user activities

--- src/components/Forms/User/components/activities/index.tsx
+++ src/components/Forms/User/components/activities/index.tsx
@@ -1,26 +1,106 @@
+import { useEffect, useState } from 'react';
+import { api } from '../../../../../api';
+import { Link, useSearchParams } from 'react-router';
+
 export default function Activities() {
+	const [searchParams] = useSearchParams();
+	const id = searchParams.get('id');
+	const [activities, setActivities] = useState([]);
+
+	const getActivities = async () => {
+		const response = await api.get('/user/activities', { params: { id } });
+		setActivities(response.data);
+	};
+
+	useEffect(() => {
+		getActivities();
+	}, []);
 	return (
 		<div className="card mb-6">
 			<h5 className="card-header">Atividade</h5>
 			<div className="card-body pt-1">
 				<ul className="timeline mb-0">
-					<li className="timeline-item timeline-item-transparent">
-						<span className="timeline-point timeline-point-primary"></span>
-						<div className="timeline-event">
-							<div className="timeline-header mb-3">
-								<h6 className="mb-0">OS N°12312312312</h6>
-								<small className="text-body-secondary">10/12/2024 10:23</small>
-							</div>
-							<p className="mb-2">Rua Arnaldo gusi 44, Xaxim Curitiba/PR</p>
-							<div className="d-flex align-items-center mb-2">
-								<div className="badge bg-lighter rounded d-flex align-items-center">
-									<span className="mb-0 text-body">Visualizar</span>
+					{activities.map((actv) => (
+						<li className="timeline-item timeline-item-transparent">
+							<span className="timeline-point timeline-point-primary"></span>
+
+							<div className="timeline-event">
+								<div className="timeline-header mb-3">
+									<h6 className="mb-0">OS N°{actv.qr_code}</h6>
+									<small className="text-body-secondary">
+										{actv.registerDay}
+									</small>
+								</div>
+								<p className="mb-2">
+									{actv.address}, {actv.neighborhood} {actv.city}/{actv.state}
+								</p>
+								<div className="d-flex align-items-center my-3">
+									<div className="badge bg-lighter rounded d-flex align-items-center">
+										<Link
+											to={`/orders/view?id=${actv.id}`}
+											className="mb-0 text-body"
+										>
+											Visualizar
+										</Link>
+									</div>
 								</div>
 							</div>
-						</div>
-					</li>
+						</li>
+					))}
 				</ul>
 			</div>
 		</div>
 	);
 }
+
+// active
+// :
+// true
+// address
+// :
+// "Ibiporã n.º:  PX83+2J"
+// city
+// :
+// "BR"
+// duplicated
+// :
+// false
+// id
+// :
+// 47
+// lat
+// :
+// "-23.2849408"
+// long
+// :
+// "-51.0459904"
+// neighborhood
+// :
+// "PR"
+// observations
+// :
+// ""
+// photoEndWork
+// :
+// "/images/endWork/prefeitura_logo.webp"
+// photoStartWork
+// :
+// "/images/startWork/link-avatar.webp"
+// protocolNumber
+// :
+// ""
+// qr_code
+// :
+// 333465
+// registerDay
+// :
+// "2025-04-14T20:22:02.564Z"
+// state
+// :
+// "AM"
+// status
+// :
+// 2
+// userId
+// :
+// "02e0fb15-9291-4952-a5d4-31f7e2cb5f35"
