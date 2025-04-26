111bea9e65b5f5d795eb08fb36173b2dc73f138e
Alexandre Oliveira <alexandre_osvaldo@hotmail.com>
2025-04-26 10:31:47 -0300

    fix image update Error

--- src/components/Forms/Image/index.tsx
+++ src/components/Forms/Image/index.tsx
@@ -9,6 +9,7 @@ export default function Image({
 }) {
 	return (
 		<img
+			key={image}
 			className="img-fluid rounded"
 			style={{
 				// maxHeight: height,
@@ -18,7 +19,7 @@ export default function Image({
 				objectFit: 'contain',
 				imageOrientation: orientation,
 			}}
-			src={`${import.meta.env.VITE_API_URL}${image}`}
+			src={`${import.meta.env.VITE_API_URL}${image}?t=${Date.now()}`}
 			// height="120"
 			// width="120"
 			alt={image}
