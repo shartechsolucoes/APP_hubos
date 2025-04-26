639ace778cf16b65b5b0065314341b3140dc2e7d
Alexandre Oliveira <alexandre_osvaldo@hotmail.com>
2025-04-26 18:01:40 -0300

    fix build

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -3,25 +3,20 @@ import Sidebar from '../components/sidebar';
 import Navbar from '../components/navbar';
 import './index.css';
 import './default.css';
-import { Outlet, useLocation, useNavigate } from 'react-router';
+import { Outlet, useNavigate } from 'react-router';
 
 import useAccessLevelStore from '../stores/accessLevelStore';
 
 export default function Layout() {
-	const location = useLocation();
 	const navigate = useNavigate();
 	const { handleAccessLevel, handleUserName, handleUserId } =
 		useAccessLevelStore();
 	const [token, setToken] = useState(localStorage.getItem('token') || '');
-	const [level, setLevel] = useState(localStorage.getItem('accessLevel') || '');
-	const [name, setName] = useState(localStorage.getItem('userName') || '');
-	const [id, setId] = useState(localStorage.getItem('userId'));
 
 	const initializer = async () => {
 		await setToken(localStorage.getItem('token') || '');
 	};
 	useEffect(() => {
-		console.log(id);
 		initializer();
 		if (!token) {
 			navigate('/login');
@@ -44,7 +39,7 @@ export default function Layout() {
 								<div className="content-wrapper">
 									<div className="container-xxl flex-grow-1 container-p-y">
 										<div className="row">
-											{id && id?.length > 0 && <Outlet />}
+											<Outlet />
 										</div>
 									</div>
 								</div>
