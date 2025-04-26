d0694fd5715a6d861b2afdc62011394eb3c86bb7
Alexandre Oliveira <alexandre_osvaldo@hotmail.com>
2025-04-26 20:59:25 -0300

    fix build

--- src/components/Forms/User/components/sideInfo/index.tsx
+++ src/components/Forms/User/components/sideInfo/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { api } from '../../../../../api';
 import useAccessLevelStore from '../../../../../stores/accessLevelStore';
 
